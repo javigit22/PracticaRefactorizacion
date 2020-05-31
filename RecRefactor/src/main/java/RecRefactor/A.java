416ab7d21a0d160a8358d4fc82ccfa798bc9cd3f
javic <javi22ar@gmail.com>
2020-05-31 22:40:00 +0200

    Copiar los métodos B y C en A

--- RecRefactor/src/main/java/RecRefactor/A.java
+++ RecRefactor/src/main/java/RecRefactor/A.java
@@ -8,6 +8,46 @@ public class A {
     static int m = 4; 
     static int n = 4; 
     
+    static void printMatriz(int[][] c, int n) 
+    { 
+        for (int i = 0; i < n; i++) { 
+            for (int j = 0; j < n; j++) {
+                System.out.print(c[i][j] + " "); 
+            }
+            System.out.println(); 
+        } 
+    } 
+    
+    static void ordenarMenorMayor(int[][] a, int n) 
+    { 
+         
+        int[] t = new int[n * n]; 
+        int k = 0; 
+      
+    
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                t[k++] = a[i][j]; 
+            }
+        }
+      
+        
+        int m = t.length;
+        for( int i=0; i < n; i++){
+            for( int j=0;j< n; j++){
+                for(int x=0; x < n; x++){
+                    for(int y=0; y < n; y++){
+                        if(a[i][j] < a[x][y]){
+                            int q = a[i][j];
+                            a[i][j] = a[x][y];
+                            a[x][y] = q;
+                        }
+                    }
+                }
+            } 
+        }       
+    }
+    
     public static void main(String[] args) {
         
         
